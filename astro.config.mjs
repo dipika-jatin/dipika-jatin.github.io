e6ff5958ce3a1887729927e3729a3d6017782d1d
dipika-jatin <jatinpandey9748@gmail.com>
2026-08-06 21:53:07 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://dipika-jatin.github.io', 
+  site: 'https://dipika-jatin.github.io',
   trailingSlash: 'ignore',
   build: {
     inlineStylesheets: 'auto',
